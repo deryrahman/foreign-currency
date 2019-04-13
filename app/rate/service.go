9902168ed6e95cb7ccdafe4887615105ae2ff4f5
Dery Rahman A <dery.ra@gmail.com>
2019-04-14 00:29:03 +0700

    feat(rate): initial service implementation

---
+++ app/rate/service.go
@@ -0,0 +1,29 @@
+package rate
+
+import "github.com/deryrahman/foreign-currency/app"
+
+// Service is struct for implementation of rate service
+type Service struct {
+	RateRepo     app.RateRepository
+	CurrencyRepo app.CurrencyRepository
+}
+
+// CreateService is a constructor for create rate service
+func CreateService(rateRepo app.RateRepository, currencyRepo app.CurrencyRepository) *Service {
+	return &Service{rateRepo, currencyRepo}
+}
+
+// CurrencyRates is a method to get currency with their rates
+// It has parameter "from", "to", and "lastNRates"
+// lastNRates < 0 will retrieve all rates, lastNRates >= 0 will retrieve recent top lastNRates
+// Before call method fetch on currency repo, "from" should less than "to" lexicographically
+func (rateService *Service) CurrencyRates(from, to string, lastNRates int) (*app.CurrencyResponse, error) {
+	return nil, nil
+}
+
+// CreateRate is a method to create daily rate
+// If currency doesn't exist yet, then create one using currency repo
+// create currency, must have "from" less than "to" lexicographically
+func (rateService *Service) CreateRate(*app.RateRequest) error {
+	return nil
+}
