7b90b8a37b789abff7deb46692a36881562f7578
Dery Rahman A <dery.ra@gmail.com>
2019-04-13 20:59:37 +0700

    test(currency): add fetchOne fail test #red

--- app/currency/rdbms_repository_test.go
+++ app/currency/rdbms_repository_test.go
@@ -3,6 +3,7 @@ package currency
 import (
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/deryrahman/foreign-currency/app"
 	"github.com/jinzhu/gorm"
@@ -30,6 +31,13 @@ func assertUint(t *testing.T, got, want uint) {
 	}
 }
 
+func assertInt(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got '%d' want '%d'", got, want)
+	}
+}
+
 func TestFetch(t *testing.T) {
 	mock, db := newDB()
 	defer db.Close()
@@ -45,4 +53,41 @@ func TestFetch(t *testing.T) {
 	assertUint(t, gots[0].ID, currencies[0].ID)
 	assertString(t, gots[0].From, currencies[0].From)
 	assertString(t, gots[0].To, currencies[0].To)
+}
+
+func TestFetchOne_full(t *testing.T) {
+	mock, db := newDB()
+	defer db.Close()
+	currencies := []app.Currency{
+		app.Currency{ID: 1, From: "USD", To: "SGD"},
+	}
+	ti := time.Now()
+	rates := []app.Rate{
+		app.Rate{ID: 1, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+		app.Rate{ID: 2, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+		app.Rate{ID: 3, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+		app.Rate{ID: 4, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+		app.Rate{ID: 5, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+		app.Rate{ID: 6, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+		app.Rate{ID: 7, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+		app.Rate{ID: 8, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+		app.Rate{ID: 9, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+		app.Rate{ID: 10, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+		app.Rate{ID: 11, Date: &ti, RateValue: 0.6, CurrencyID: 1},
+	}
+	rowsCurrencies := mock.NewRows([]string{"id", "from", "to"}).
+		AddRow(currencies[0].ID, currencies[0].From, currencies[0].To)
+	rowsRates := mock.NewRows([]string{"id", "date", "rate_value", "currency_id"})
+	for i := range rates {
+		rowsRates = rowsRates.AddRow(rates[i].ID, rates[i].Date, rates[i].RateValue, rates[i].CurrencyID)
+	}
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `currencies` WHERE (from = ? AND to = ?) ORDER BY `currencies`.`id` ASC LIMIT 1")).WillReturnRows(rowsCurrencies)
+	mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM `rates`  WHERE (`currency_id` = ?) ORDER BY rates.date DESC,`rates`.`id` ASC")).WillReturnRows(rowsRates)
+
+	repo := CreateRDBMSRepo(db)
+	got, _ := repo.FetchOne("USD", "SGD", -1)
+	assertUint(t, got.ID, currencies[0].ID)
+	assertString(t, got.From, currencies[0].From)
+	assertString(t, got.To, currencies[0].To)
+	assertInt(t, len(got.Rates), 11)
 }
