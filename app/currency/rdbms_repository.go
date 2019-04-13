5020a5174702b7886f5abfcde5cbc13b7c21354f
Dery Rahman A <dery.ra@gmail.com>
2019-04-13 20:29:47 +0700

    feat(currency): init implementation currency repo

---
+++ app/currency/rdbms_repository.go
@@ -0,0 +1,36 @@
+package currency
+
+import (
+	"github.com/deryrahman/foreign-currency/app"
+	"github.com/jinzhu/gorm"
+)
+
+// RDBMSRepo is a struct to wrap its DB
+type RDBMSRepo struct {
+	DB *gorm.DB
+}
+
+// CreateRDBMSRepo is used to create new Mysql repository
+func CreateRDBMSRepo(db *gorm.DB) *RDBMSRepo {
+	return &RDBMSRepo{db}
+}
+
+// Fetch is a method to fetch all currency that match with query
+// It should return ErrNotFound if currency didn't found
+func (repo *RDBMSRepo) Fetch() ([]*app.Currency, error) {
+	return nil, nil
+}
+
+// FetchOne is a method to fetch one currency pair
+// It should pass parameter "from", "to", and "lastNRates"
+// "from" is always less than "to" lexicographically
+// If lastNRates is negative, return all Rates, get latest N rates otherwise
+// It should return ErrNotFound if currency didn't found
+func (repo *RDBMSRepo) FetchOne(from, to string, lastNRates int) (*app.Currency, error) {
+	return nil, nil
+}
+
+// Store is a method to store new currency into database
+func (repo *RDBMSRepo) Store(currency *app.Currency) error {
+	return nil
+}
