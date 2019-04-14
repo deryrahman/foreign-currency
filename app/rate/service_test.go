d2dd4ee20462cf9308d8cf27cb60797f4fff904e
Dery Rahman A <dery.ra@gmail.com>
2019-04-14 07:00:33 +0700

    test(rate): add failing test currency rates on service #red

---
+++ app/rate/service_test.go
@@ -0,0 +1,57 @@
+package rate
+
+import (
+	"testing"
+	"time"
+
+	"github.com/deryrahman/foreign-currency/app"
+)
+
+type RateRepoMock struct {
+	FetchFn            bool
+	FetchBetweenDateFn bool
+	StoreFn            bool
+}
+type CurrencyRepoMock struct {
+	FetchFn    bool
+	FetchOneFn bool
+	StoreFn    bool
+}
+
+func (repo *CurrencyRepoMock) Fetch() ([]*app.Currency, error) {
+	repo.FetchFn = true
+	return nil, nil
+}
+func (repo *CurrencyRepoMock) FetchOne(from, to string, lastNRates int) (*app.Currency, error) {
+	repo.FetchOneFn = true
+	return nil, nil
+}
+func (repo *CurrencyRepoMock) Store(*app.Currency) error {
+	repo.StoreFn = true
+	return nil
+}
+
+func (repo *RateRepoMock) Fetch() ([]*app.Rate, error) {
+	repo.FetchFn = true
+	return nil, nil
+}
+func (repo *RateRepoMock) FetchBetweenDate(*time.Time, *time.Time) ([]*app.Rate, error) {
+	repo.FetchBetweenDateFn = true
+	return nil, nil
+}
+func (repo *RateRepoMock) Store(*app.Rate) error {
+	repo.StoreFn = true
+	return nil
+}
+func TestCurrencyRates(t *testing.T) {
+	rateRepo := &RateRepoMock{false, false, false}
+	currencyRepo := &CurrencyRepoMock{false, false, false}
+	rateService := CreateService(rateRepo, currencyRepo)
+
+	rateService.CurrencyRates("USD", "SGD", 7)
+	got := currencyRepo.FetchOneFn
+	want := true
+	if got != want {
+		t.Errorf("got '%v' want '%v'", got, want)
+	}
+}
