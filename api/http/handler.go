e0068d2cd6f5324af950b08f704ddfd67137db74
Dery Rahman A <dery.ra@gmail.com>
2019-04-14 19:56:16 +0700

    feat(http): add logger

--- api/http/handler.go
+++ api/http/handler.go
@@ -2,6 +2,7 @@ package customhttp
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/deryrahman/foreign-currency/app"
@@ -24,6 +25,7 @@ func CreateHTTPHandler(rateService app.RateService, trackService app.TrackServic
 // GetRates is a method to get rates
 // It receive query "from" and "to", and will retrive corresponding data of rates details
 func (h *HTTPHandler) GetRates(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET rates")
 	w.Header().Set("Content-Type", "application/json")
 	queries := r.URL.Query()
 	from := queries.Get("from")
@@ -47,6 +49,7 @@ func (h *HTTPHandler) GetRates(w http.ResponseWriter, r *http.Request) {
 // PostRates is a method to create daily rates
 // It will read and parse request body as json and marshaling into RateRequest model
 func (h *HTTPHandler) PostRates(w http.ResponseWriter, r *http.Request) {
+	log.Println("POST rates")
 	w.Header().Set("Content-Type", "application/json")
 	decoder := json.NewDecoder(r.Body)
 	rateRequest := app.RateRequest{}
@@ -75,6 +78,7 @@ func (h *HTTPHandler) PostRates(w http.ResponseWriter, r *http.Request) {
 // GetTracks is a method to get all tracks
 // It receive query "date" with format YYYY-MM-DD
 func (h *HTTPHandler) GetTracks(w http.ResponseWriter, r *http.Request) {
+	log.Println("GET tracks")
 	w.Header().Set("Content-Type", "application/json")
 	queries := r.URL.Query()
 	date := queries.Get("date")
@@ -97,6 +101,7 @@ func (h *HTTPHandler) GetTracks(w http.ResponseWriter, r *http.Request) {
 // PostTracks is a method to invoke currency rate to be tracked
 // It will read request body as json. Json parameter are "from" and "to"
 func (h *HTTPHandler) PostTracks(w http.ResponseWriter, r *http.Request) {
+	log.Println("POST tracks")
 	w.Header().Set("Content-Type", "application/json")
 	decoder := json.NewDecoder(r.Body)
 	trackRequest := app.TrackRequest{}
@@ -125,6 +130,7 @@ func (h *HTTPHandler) PostTracks(w http.ResponseWriter, r *http.Request) {
 // DeleteTracks is a method to remove currency rate to be tracked
 // It receive query "from" and "to"
 func (h *HTTPHandler) DeleteTracks(w http.ResponseWriter, r *http.Request) {
+	log.Println("DELETE tracks")
 	w.Header().Set("Content-Type", "application/json")
 	queries := r.URL.Query()
 	from := queries.Get("from")
