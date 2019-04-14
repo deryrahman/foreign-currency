f7ec9ae9a66dd1c42a33775784d28343f81f075c
Dery Rahman A <dery.ra@gmail.com>
2019-04-14 08:38:59 +0700

    test(track): add failing test create track when it exist #red

--- app/track/rdbms_repository_test.go
+++ app/track/rdbms_repository_test.go
@@ -123,3 +123,46 @@ func TestStore(t *testing.T) {
 		assertUint(t, gots[i].CurrencyID, tracks[i].CurrencyID)
 	}
 }
+
+func TestStore_exist(t *testing.T) {
+	db := newDB(t)
+	defer db.Close()
+
+	currencies := []app.Currency{
+		app.Currency{
+			From: "SGD",
+			To:   "USD",
+		},
+		app.Currency{
+			From: "IDR",
+			To:   "USD",
+		},
+		app.Currency{
+			From: "JPY",
+			To:   "USD",
+		},
+	}
+	for i := range currencies {
+		db.Create(&currencies[i])
+	}
+	tracks := []app.Track{
+		app.Track{
+			CurrencyID: currencies[0].ID,
+		},
+		app.Track{
+			CurrencyID: currencies[1].ID,
+		},
+	}
+	for i := range tracks {
+		db.Create(&tracks[i])
+	}
+
+	repo := CreateRDBMSRepo(db)
+	for i := range tracks {
+		tracks[i].ID = 0
+		err := repo.Store(&tracks[i])
+		if err == nil {
+			t.Errorf("wanted an error")
+		}
+	}
+}
